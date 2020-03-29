c9438c7a3c8ade1707949b9da09bbcdeab569cea
Fred C <canofred@gmail.com>
2020-03-29 00:49:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,11 +19,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  if hash[key]
-    return hash[:key] += 1 
-  else 
-    return hash[:key] = 1
-  return hash[key]
+  # return the correct value using the hash and key parameters
 end
 
 def update_counting_hash(hash, key)
