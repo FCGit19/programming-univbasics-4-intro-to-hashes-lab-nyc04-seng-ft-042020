4631da82487d213af0bd4cf7dee0aee799871f86
Fred C <canofred@gmail.com>
2020-03-29 01:12:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[:key]
-    puts hash[:key] += 1 
+  if hash[key]
+    puts hash[key] += 1 
   else 
-    puts hash[:key] = 1
+    puts hash[key] = 1
   end
   return hash
 end
